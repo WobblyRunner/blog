bd0b7108694b0f832e81668d5af017c2fb7b248a
joshua.m.willis8 <joshua.m.willis8@gmail.com>
2022-07-02 10:15:02 -0700

    Added models. Added Bootstrap and modified libman. Added gobalusings.cs

---
+++ Blog/Data/Contexts/BlogContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+using Blog.Data.Models;
+
+namespace Blog.Data.Contexts;
+
+public class BlogContext : DbContext
+{
+	public DbSet<BlogPost> BlogPosts { get; set; } = null!;
+
+
+}
---
+++ Blog/Data/Models/BlogPost.cs
@@ -0,0 +1,22 @@
+﻿namespace Blog.Data.Models;
+
+// Disabling nullable warnings ; all of this is data as a class, should not require default values.
+#nullable disable
+
+public class BlogPost
+{
+	/// <summary> Primary Key as GUID </summary>
+	public Guid PostID { get; set; }
+
+	/// <summary> Title of the blog post </summary>
+	[Required] [StringLength(60)] public string Title { get; set; }
+	/// <summary> Contents of the blog post </summary>
+	[Required] [StringLength(4_000)] public string Content { get; set; }
+
+	/// <summary> Blog post descriptive tags </summary>
+	[StringLength(600)] public string Tags { get; set; }
+
+	private const int TWENTYFOUR_MEG = 1028*1028*24;
+	/// <summary> Thumbnail image of the blog post </summary>
+	[MaxLength(TWENTYFOUR_MEG)]public byte[] Image { get; set; }
+}
