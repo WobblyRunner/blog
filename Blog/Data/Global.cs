ca549b99a779000ba07002c53d71db1c967494c7
joshua.m.willis8 <joshua.m.willis8@gmail.com>
2022-07-03 20:04:27 -0700

    Set up temp storage of file blob. Needs chunking of file when sending to SQL Server.

--- Blog/Data/Global.cs
+++ Blog/Data/Global.cs
@@ -1,11 +1,5 @@
 ﻿namespace Blog.Data;
 
-public static class Global
+internal static class Global
 {
-	public const string ENV_UNTRUSTEDCACHE_FILEPATH = @"untrusted_random_cache";
-	public static string GetRandomCachedFilePath(this IHostEnvironment environment, out string randomFileName)
-	{
-		randomFileName = Path.GetRandomFileName();
-		return Path.Combine(environment.ContentRootPath, environment.EnvironmentName, ENV_UNTRUSTEDCACHE_FILEPATH, randomFileName);
-	}
 }
